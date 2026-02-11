7d673f14ef3d05d6875554ce2a19d7b82848b554
electronicafranko-creator <electronicafranko@gmail.com>
2026-02-11 11:49:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
-// --- SISTEMA DE NAVEGACIÓN POR CAPAS ---
+// --- SISTEMA DE NAVEGACIÓN DE FRANKO ---
 
+// Función para entrar a una categoría (Nivel 2)
 function abrirSubventana(id) {
     document.getElementById('ventana-inicio').style.display = 'none';
     document.getElementById(id).style.display = 'block';
     window.scrollTo(0, 0);
 }
 
+// Función para volver al Menú Principal
 function cerrarSubventana(id) {
     document.getElementById(id).style.display = 'none';
     document.getElementById('ventana-inicio').style.display = 'block';
 }
 
+// Función para entrar a la herramienta final (Nivel 3)
 function abrirHerramienta(id) {
     document.getElementById('cat-conexiones').style.display = 'none';
     document.getElementById(id).style.display = 'block';
     window.scrollTo(0, 0);
 }
 
+// Función para volver a la categoría de Conexiones
 function cerrarHerramienta(id) {
     document.getElementById(id).style.display = 'none';
     document.getElementById('cat-conexiones').style.display = 'block';
