f0a27569aa5a75ab6f7f617eb427f442fb5d080b
electronicafranko-creator <electronicafranko@gmail.com>
2026-02-11 11:34:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 // --- SISTEMA DE NAVEGACIÓN POR CAPAS ---
 
-// 1. Abrir una categoría (Nivel 2) desde el Inicio
 function abrirSubventana(id) {
     document.getElementById('ventana-inicio').style.display = 'none';
     document.getElementById(id).style.display = 'block';
-    window.scrollTo(0, 0); // Sube al inicio de la página
+    window.scrollTo(0, 0);
 }
 
-// 2. Volver al Inicio desde una categoría
 function cerrarSubventana(id) {
     document.getElementById(id).style.display = 'none';
     document.getElementById('ventana-inicio').style.display = 'block';
 }
 
-// 3. Abrir herramienta específica (Nivel 3)
 function abrirHerramienta(id) {
-    // Ocultamos la categoría de calculadoras
-    document.getElementById('cat-calculadoras').style.display = 'none';
+    document.getElementById('cat-conexiones').style.display = 'none';
     document.getElementById(id).style.display = 'block';
     window.scrollTo(0, 0);
 }
 
-// 4. Volver a la categoría desde la herramienta
 function cerrarHerramienta(id) {
     document.getElementById(id).style.display = 'none';
-    document.getElementById('cat-calculadoras').style.display = 'block';
+    document.getElementById('cat-conexiones').style.display = 'block';
 }
